5c69e5297a0bfb403147667d71b6bc569dcb3a12
Michael Eboagu <Michael Eboagu>
2017-05-15 16:28:41 +0100

    add readme

--- config/express.js
+++ config/express.js
@@ -2,3 +2,23 @@ const express = require('express');
 const morgan = require('morgan');
 const compress = require('compression');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const app = express();
+
+if(process.env.NODE_ENV == "development"){
+	app.use(morgan('dev'));
+}
+
+else if(process.env.NODE_ENV == "production"){
+	app.use(compress());
+}
+
+
+app.use(bodyParser.urlencoded({extended: true}));  
+app.use(bodyParser.json());  
+app.use(methodOverride());
+
+require('../app/routes/index')(app);
+
+module.exports = app;
