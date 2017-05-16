969d06264872b284e87795373d3ad6ce1663b4b0
Michael Eboagu <Michael Eboagu>
2017-05-16 16:12:47 +0100

    production database

---
+++ config/env/production.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // Development configuration options 
+  db: 'mongodb://supermike:michael2017@host1:port1,host2:port2/photo-editor',
+  sessionSecret: 'myDevelopmentSecret' 
+};
+
